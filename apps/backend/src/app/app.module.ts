130ab42155d74c7349a896ef453202ec0b2d17d0
praktyki <jactyl13@gmail.com>
2024-02-13 15:29:36 +0100

    Setup backend

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-
+import { ThrottlerModule } from '@nestjs/throttler'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 4,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 15,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,18 +1,25 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import helmet from 'helmet'
 import { AppModule } from './app/app.module';
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser())
+  app.enableCors({
+    origin: process.env.TARGET_SITE || 'localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+  app.use(helmet())
+
   const port = process.env.PORT || 4000;
+  
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
