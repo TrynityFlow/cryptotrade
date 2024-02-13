ca757d81a216955b5922c98bb729402d376a8d19
praktyki <jactyl13@gmail.com>
2024-02-13 15:30:18 +0100

    Format

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ThrottlerModule } from '@nestjs/throttler'
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,25 +1,25 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import helmet from 'helmet'
+import helmet from 'helmet';
 import { AppModule } from './app/app.module';
-import * as cookieParser from 'cookie-parser'
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  
+
   app.useGlobalPipes(new ValidationPipe());
-  app.use(cookieParser())
+  app.use(cookieParser());
   app.enableCors({
     origin: process.env.TARGET_SITE || 'localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  })
-  app.use(helmet())
+  });
+  app.use(helmet());
 
   const port = process.env.PORT || 4000;
-  
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
