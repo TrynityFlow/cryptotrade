8764376b89d77d02959f8eaf34470ef69109f1d1
praktyki <jactyl13@gmail.com>
2024-02-14 10:43:04 +0100

    Create local strategy

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { User } from '../types';
 
 @Injectable()
 export class AuthService {
@@ -8,7 +9,7 @@ export class AuthService {
   async validateUser(
     username: string,
     pass: string,
-  ): Promise<{ id: number; username: string } | null> {
+  ): Promise<User | null> {
     const user = await this.usersService.findByUserName(username);
     if (user && user.password.toString() == pass) {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
