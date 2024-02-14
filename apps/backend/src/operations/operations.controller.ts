5562a6e2c9fbcbf834c29ccbcc89c562ece7f28c
praktyki <jactyl13@gmail.com>
2024-02-14 14:20:25 +0100

    Creat dto for operation

--- apps/backend/src/operations/operations.controller.ts
+++ apps/backend/src/operations/operations.controller.ts
@@ -1,12 +1,15 @@
 import {
+    Body,
   Controller,
   Get,
   InternalServerErrorException,
+  Post,
   Req,
   UseGuards,
 } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt.guard';
 import { OperationsService } from './operations.service';
+import { CreateOpDto } from './operations.dto';
 
 @Controller('operations')
 export class OperationsController {
@@ -18,5 +21,11 @@ export class OperationsController {
   async getOps(@Req() req: any) {
     if (!req.user.id) throw new InternalServerErrorException();
     return this.opService.getAllOpsOfUser(req.user.id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async createOp(@Body() payload: CreateOpDto) {
+    payload
   }
 }
---
+++ apps/backend/src/operations/operations.dto.ts
@@ -0,0 +1,18 @@
+import { IsBoolean, IsNotEmpty, IsNumber, IsPositive, IsString } from "class-validator";
+
+export class CreateOpDto {
+    @IsString()
+    @IsNotEmpty()
+    currency_id: string;
+
+    @IsBoolean()
+    sell: boolean
+
+    @IsNumber()
+    @IsPositive()
+    amount: number
+
+    @IsNumber()
+    @IsPositive()
+    price: number
+}
