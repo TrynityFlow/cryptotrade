2cbfbb138cbbc10228a1eec7002a7006becd2786
seba-the-programmer <megaseba9@gmail.com>
2024-02-15 14:13:07 +0100

    Create wallet service

--- apps/backend/src/wallet/wallet.controller.ts
+++ apps/backend/src/wallet/wallet.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt.guard';
+import { WalletService } from './wallet.service';
 
 @Controller('wallet')
-export class WalletController {}
+export class WalletController {
+    constructor(private readonly walletService: WalletService) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    async getBalance(@Req() req: any) {
+        return await this.walletService.getBalance(req.user.id as number)
+    }
+}
--- apps/backend/src/wallet/wallet.service.ts
+++ apps/backend/src/wallet/wallet.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { Balance } from '../types';
 
 @Injectable()
 export class WalletService {
@@ -7,19 +8,58 @@ export class WalletService {
 
   async getBalance(userId: number) {
     const group = await this.prisma.operation.groupBy({
+      by: ['currency_id', 'sell'],
+      where: {
+        user_id: userId,
+      },
+      _sum: {
+        price: true,
+        amount: true
+      },
+      _avg: {
+        price: true
+      },
+    });
+
+    if(!group) return {}
+
+    const cryptoMap = new Map<string, Balance>()
+    group.map(current => {
+      const id = current.currency_id
+      if(!cryptoMap.has(id)) cryptoMap.set(id, {balance: 0, amount: 0, avg_sell: 0, avg_buy: 0})
+
+      cryptoMap.get(id).balance += (current._sum.price * (current.sell ? -1 : 1))
+      cryptoMap.get(id).amount += (current._sum.amount * (current.sell ? -1 : 1))
+
+      if(current.sell) {
+        cryptoMap.get(id).avg_sell = current._avg.price
+        return
+      }
+      cryptoMap.get(id).avg_buy = current._avg.price
+    })
+    return Object.fromEntries(cryptoMap)
+  }
+
+  async getBalanceById(userId: number, currId: string) {
+    const group = await this.prisma.operation.groupBy({
       by: ['sell'],
       where: {
         user_id: userId,
+        currency_id: currId
       },
       _sum: {
         price: true,
       },
     });
 
-    return group[0]._sum;
+    if(!group) return 0
+    return group.reduce((acc, current) => {
+      return acc + (current._sum.price * (current.sell ? -1 : 1))
+    }, 0)
   }
 
-  // async getBalanceById(userId: number, currId: string) {
-
-  // }
+  async checkSell(userId: number, currId: string, price: number): Promise<boolean> {
+    const res = await this.getBalanceById(userId, currId)
+    return (res - price) > 0
+  }
 }
