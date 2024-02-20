d44c151265eb80070c5502ab9e8fef72efe85087
praktyki <jactyl13@gmail.com>
2024-02-20 13:36:21 +0100

    Add simple login page

--- apps/frontend/src/components/login/loginImage.tsx
+++ apps/frontend/src/components/login/loginImage.tsx
@@ -2,7 +2,7 @@ import { LoginNav } from './loginNav';
 
 export const LoginImage = () => {
   return (
-    <div className="bg-background hidden h-screen w-7/12 flex-col items-center justify-center md:flex">
+    <div className="hidden h-screen w-7/12 flex-col items-center justify-center bg-transparent md:flex">
       <LoginNav />
       <p className="mt-4 w-8/12 px-8 py-2 text-center text-xl font-medium">
         &quot;Every industry has actors good and bad. Crypto is no exception.
--- apps/frontend/src/layouts/login.layout.tsx
+++ apps/frontend/src/layouts/login.layout.tsx
@@ -2,7 +2,7 @@ import { LoginForm, LoginImage } from '../components';
 
 export const LoginLayout = () => {
   return (
-    <div className="h-screen overflow-hidden">
+    <div className="bg-background h-screen overflow-hidden">
       <div className="flex h-full w-full items-center justify-start">
         <LoginImage />
         <LoginForm />
