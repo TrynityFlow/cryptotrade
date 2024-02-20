c6f00a5f7a5c6e1119618bcdcd9f26a25c7257ce
Misunderstnd <142392135+OriginalCola@users.noreply.github.com>
2024-02-20 13:39:34 +0100

    Modal

---
+++ apps/frontend/src/components/elements/Modal.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from "@nextui-org/react";
+
+
+export default function ModalObj(Title: string, Text: string) {
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
+
+  return (
+    <>
+      <Button onPress={onOpen}>Open Modal</Button>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top">
+        <ModalContent>
+          {(onClose: any) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">{Title}</ModalHeader>
+              <ModalBody>
+                <p> 
+                    {Text}
+                </p>
+
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
--- apps/frontend/src/layouts/login.layout.tsx
+++ apps/frontend/src/layouts/login.layout.tsx
@@ -1,11 +1,12 @@
 import { LoginForm, LoginImage } from '../components';
-
+import ModalObj from '../components/elements/Modal';
 export const LoginLayout = () => {
   return (
     <div className="h-screen overflow-hidden">
       <div className="flex h-full w-full items-center justify-start">
         <LoginImage />
         <LoginForm />
+        <ModalObj />
       </div>
     </div>
   );
