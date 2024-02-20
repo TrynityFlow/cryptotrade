eefa2fe9793e04bbd3b15c83f7d3538f9d92cade
praktyki <jactyl13@gmail.com>
2024-02-20 13:44:06 +0100

    Add login nav

--- apps/frontend/src/layouts/login.layout.tsx
+++ apps/frontend/src/layouts/login.layout.tsx
@@ -1,8 +1,9 @@
-import { LoginForm, LoginImage } from '../components';
+import { LoginForm, LoginImage, LoginNav } from '../components';
 
 export const LoginLayout = () => {
   return (
-    <div className="bg-background h-screen overflow-hidden">
+    <div className="bg-background h-screen overflow-hidden px-2 py-4 md:px-8">
+      <LoginNav />
       <div className="flex h-full w-full items-center justify-start">
         <LoginImage />
         <LoginForm />
