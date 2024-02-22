bf92da963847f9a98a0fad8efc675cae7f04c2b5
praktyki <jactyl13@gmail.com>
2024-02-22 12:27:03 +0100

    Add writing to storage

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,11 +1,7 @@
-import { useContext } from 'react';
 import { MainLayout } from '../layouts';
-import { LoginContext } from '../libs/loginContext';
 
 export function Index() {
-  const { user } = useContext(LoginContext);
-
-  return <MainLayout>{user?.username}</MainLayout>;
+  return <MainLayout></MainLayout>;
 }
 
 export default Index;
