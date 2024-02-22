8add3abce545316a0e806ef0b44244689a699d96
praktyki <jactyl13@gmail.com>
2024-02-22 14:57:01 +0100

    Add login context provider

--- apps/frontend/src/libs/loginContext.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const LoginContext = React.createContext<Crypto.LoginContext>({
-  user: undefined,
-  updateUser: (user: Request.User) => {
-    return;
-  },
-});
