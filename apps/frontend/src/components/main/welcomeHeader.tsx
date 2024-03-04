6807a654c1ca3c5e48e039271befa04e69525388
praktyki <jactyl13@gmail.com>
2024-03-04 09:08:08 +0100

    Fix header styling

--- apps/frontend/src/components/main/welcomeHeader.tsx
+++ apps/frontend/src/components/main/welcomeHeader.tsx
@@ -8,7 +8,7 @@ export const WelcomeHeader = () => {
   }
 
   return (
-    <h2 className="text-2xl font-semibold">
+    <h2 className="text-3xl font-semibold mb-14 mt-4 text-center">
       HI, {user?.username.toUpperCase()}!
     </h2>
   );
