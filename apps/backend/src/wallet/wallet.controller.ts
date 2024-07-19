da5e443ae6e721fea97432390029da41f8976635
mtw-programmer <visnia58@gmail.com>
2024-07-19 13:12:39 +0200

    docs(wallet): create docs

--- apps/backend/src/wallet/wallet.controller.ts
+++ apps/backend/src/wallet/wallet.controller.ts
@@ -2,13 +2,34 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt.guard';
 import { WalletService } from './wallet.service';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+import {
+  InternalServerError,
+  Unauthorized,
+} from '../docs/common/responses.docs';
 
+@ApiTags('Wallet')
 @Controller('wallet')
 export class WalletController {
   constructor(private readonly walletService: WalletService) {}
 
   @UseGuards(JwtAuthGuard)
   @Get()
+  @ApiOperation({ summary: 'Get user balance' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfull get balance',
+    schema: {
+      properties: {
+        balance: {
+          type: 'string',
+          example: '1.00000000001'
+        },
+      },
+    },
+  })
+  @ApiResponse(Unauthorized)
+  @ApiResponse(InternalServerError)
   async getBalance(@Req() req: any) {
     const balance = await this.walletService.getBalance(req.user.id as number);
     return { balance };
